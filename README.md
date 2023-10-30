9f431a13a874ef2f4d9f697f066c1072d58a8a3b
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2023-10-30 06:39:38 +0000

    give up using gpu inference, instead using custom container

--- README.md
+++ README.md
@@ -11,4 +11,6 @@ Inference 在部署時主要涵蓋四個項目：
 1. Inference 範例：https://sagemaker-examples.readthedocs.io/en/latest/frameworks/pytorch/get_started_mnist_deploy.html
 2. 內建 Image uri：https://docs.aws.amazon.com/zh_tw/sagemaker/latest/dg/notebooks-available-images.html
 3. https://www.youtube.com/watch?v=YQyid2uLOvI
-4. sagemaker image uri gpu serve issue: https://huggingface.co/sanchit-gandhi/whisper-medium-fleurs-lang-id/discussions/2
+4. Github Image URI（主要用這個）：https://github.com/aws/deep-learning-containers/blob/master/available_images.md
+5. sagemaker image uri gpu serve issue: https://huggingface.co/sanchit-gandhi/whisper-medium-fleurs-lang-id/discussions/2
+6. paddlenlp uie sagemaker example: https://aws.amazon.com/cn/blogs/china/training-a-model-based-on-amazon-sagemaker-for-keyword-extraction-and-deployment/
--- src/inference.py
+++ src/inference.py
@@ -9,8 +9,6 @@ from paddlenlp import Taskflow
 # 必須包含的方法
 ## 1. 載入參數：load checkpoints with fixed-name function called 'model_fn'
 def model_fn(model_dir: str):
-    print("hahaha: in model_fn")
-    print("model_dir: ", model_dir)
     task_path = os.path.join(model_dir, "model_best")
 
     model = Taskflow(
@@ -19,6 +17,7 @@ def model_fn(model_dir: str):
         task_path=task_path,
         precision="fp32",
     )
+
     print("model: ", model)
     return model
 
@@ -26,8 +25,7 @@ def model_fn(model_dir: str):
 ## 2. input 前處理
 def input_fn(request_body, request_content_type):
     assert request_content_type == "application/json"
-    result = []
-    print("hahaha: in input_fun")
+
     print("request_body: ", request_body)
     data = json.loads(request_body)["inputs"]
     print("data: ", data)
@@ -37,9 +35,11 @@ def input_fn(request_body, request_content_type):
 
 ## 3. 過模型
 def predict_fn(input_object, model):
-    print("hahaha in predict_fn")
-    print("input_object: ", input_object)
-    predict_result = model(input_object)
+    predict_result = ""
+    try:
+        predict_result = model(input_object)
+    except Exception as e:
+        print("CustomizeMsg: ", str(e))
     print("predict_result: ", predict_result)
     return predict_result
 
@@ -48,102 +48,3 @@ def predict_fn(input_object, model):
 def output_fn(prediction, content_type):
     print("hahaha: in output_fn")
     return json.dumps(prediction)
-
-
-"""
-def inference(
-    data_file: str,
-    schema: List[str],
-    device_id: int = 0,
-    text_list: List[str] = None,
-    precision: str = "fp32",
-    batch_size: int = 1,
-    model: str = "uie-base",
-    task_path: str = None,
-    postprocess_fun: Callable = lambda x: x,
-    preprocess_fun: Callable = lambda x: x,
-):
-    if not os.path.exists(data_file) and not text_list:
-        raise ValueError(f"Data not found in {data_file}. Please input the correct path of data.")
-
-    if task_path:
-        if not os.path.exists(task_path):
-            raise ValueError(f"{task_path} is not a directory.")
-
-        uie = Taskflow(
-            "information_extraction",
-            schema=schema,
-            task_path=task_path,
-            precision=precision,
-            batch_size=batch_size,
-            device_id=device_id,
-        )
-    else:
-        uie = Taskflow(
-            "information_extraction",
-            schema=schema,
-            model=model,
-            precision=precision,
-            batch_size=batch_size,
-            device_id=device_id,
-        )
-
-    if not text_list:
-        with open(data_file, "r", encoding="utf8") as f:
-            text_list = [line.strip() for line in f]
-
-    return postprocess_fun([uie(preprocess_fun(text)) for text in tqdm(text_list)])
-
-
-if __name__ == "__main__":
-    parser = PdArgumentParser((InferenceDataArguments, InferenceStrategyArguments, InferenceTaskflowArguments))
-    data_args, strategy_args, taskflow_args = parser.parse_args_into_dataclasses()
-
-    uie_processer = Processer(
-        select_strategy=strategy_args.select_strategy,
-        threshold=strategy_args.select_strategy_threshold,
-        select_key=strategy_args.select_key,
-        is_regularize_data=data_args.is_regularize_data,
-    )
-
-    logger.info("Start Inference...")
-
-    inference_result = inference(
-        data_file=data_args.data_file,
-        device_id=taskflow_args.device_id,
-        schema=entity_type,
-        text_list=data_args.text_list,
-        precision=taskflow_args.precision,
-        batch_size=taskflow_args.batch_size,
-        model=taskflow_args.model,
-        task_path=taskflow_args.task_path,
-        postprocess_fun=uie_processer.postprocess,
-        preprocess_fun=uie_processer.preprocess,
-    )
-
-    logger.info("========== Inference Results ==========")
-    for i, text_inference_result in enumerate(inference_result):
-        logger.info(f"========== Content {i} Results ==========")
-        logger.info(text_inference_result)
-    logger.info("End Inference...")
-
-    if data_args.save_dir:
-        out_result = []
-        if not os.path.exists(data_args.save_dir):
-            logger.warning(f"{data_args.save_dir} is not found. Auto-create the dir.")
-            os.makedirs(data_args.save_dir)
-
-        with open(data_args.data_file, "r", encoding="utf8") as f:
-            text_list = [line.strip() for line in f]
-
-        with open(os.path.join(data_args.save_dir, data_args.save_name), "w", encoding="utf8") as f:
-            for content, result in zip(text_list, inference_result):
-                out_result.append(
-                    {
-                        "Content": content,
-                        "InferenceResults": result,
-                    }
-                )
-            jsonString = json.dumps(out_result, ensure_ascii=False)
-            f.write(jsonString)
-"""
